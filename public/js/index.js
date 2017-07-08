913a6bff8e8348b23602e3a9443536fdcf0fc8f6
vaibhavshah96 <vaibhavshah96@gmail.com>
2017-07-08 19:28:33 +0530

    added click function to add button
    type text in field and press add, it is added to the list.
    dounle click on the added item to remove it.

--- public/js/index.js
+++ public/js/index.js
@@ -40,3 +40,32 @@ document.getElementById('link-five').addEventListener('click', function () {
 }, false);
 
 show(document.getElementById('one'));
+
+
+
+$(document).ready(
+    function(){
+        $('#button').click(
+            function(){
+                var toAdd = $('input[name=ListItem]').val();
+                 $('ol').append('<li>' + toAdd + '</li>');
+            });
+       
+       $("input[name=ListItem]").keyup(function(event){
+          if(event.keyCode == 13){
+            $("#button").click();
+          }         
+      });
+      
+      $(document).on('dblclick','li', function(){
+        $(this).toggleClass('strike').fadeOut('slow');    
+      });
+      
+      $('input').focus(function() {
+        $(this).val('');
+      });
+      
+      $('ol').sortable();  
+      
+    }
+);
